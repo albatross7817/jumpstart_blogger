86917f87d2c14dfe4c6613522f0d42a47b568059
Christian Alic Kelley <cakelley@colorado.edu>
2015-02-23 03:45:33 +0000

    Updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ end
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'drinks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
