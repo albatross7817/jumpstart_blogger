b1304c777e18ae1f961d89f20d5c7e9c0c091cc3
Christian Alic Kelley <cakelley@colorado.edu>
2015-02-23 03:23:59 +0000

    added a few gems

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222194553) do
+ActiveRecord::Schema.define(version: 20150223021857) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: true do |t|
